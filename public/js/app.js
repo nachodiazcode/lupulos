aea13a09a5add30209e7341e02c5ce9ad122855d
nachodiaz <ignaciosergiodiaz@gmail.com>
2019-03-06 15:20:46 -0300

    ignaciosergiodiaz@gmail.com

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.fixed-action-btn').floatingActionButton();
+
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration:255,
+        constrainWidth: false,
+        hover: true ,
+        gutter: 0,
+        belowOrigin: true,
+        alignment: 'left',
+        stopPropagation: false
+    });
+
 });
-        
+ 
 
-$('.dropdown-button').dropdown({
-    inDuration: 300,
-    outDuration:255,
-    constrainWidth: false,
-    hover: true ,
-    gutter: 0,
-    belowOrigin: true,
-    alignment: 'left',
-    stopPropagation: false
-})
+window.onload = function ocultar_registro(){
+    var formulario2 = document.getElementById("formulario2");
+  }
 
+  function mostrar(){
+    document.getElementById("formulario2").style.display = "block" ;
+    document.getElementById("formulario").style.display = "none" ;
+  }
+
+  function ocultar(){
+    document.getElementById("formulario").style.display = "block" ;
+    document.getElementById("formulario2").style.display = "none" ;
+  }
+
+  function mostrar_ocultar(){
+
+    if(formulario2.style.display == "none"){
+      mostrar();
+    } else {
+      ocultar();
+    }
+
+  }
