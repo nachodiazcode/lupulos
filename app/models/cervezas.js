a1cc8f13369959447be24f4d3d300e4d432c9b6d
Ignacio Díaz <nachodiaz@Ignacios-MacBook-Pro.local>
2020-03-20 18:29:31 -0300

    discard change master

--- app/models/cervezas.js
+++ app/models/cervezas.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose')
-const   Schema = mongoose.Schema
+const Schema = mongoose.Schema
 
 const Cervezas = new Schema({
-
   titulo: {type:String},
   descripcion: {type: String},
-  imagen: {type: String},
+  imagen:  {type: String},
   usuario: {type: Schema.Types.ObjectId, ref:'usuarios'},
   username:{type: String, ref:'usuarios'},
-
-
 } , { timestamps: true } ) 
-
 
 module.exports = mongoose.model('cervezas', Cervezas) 
 
